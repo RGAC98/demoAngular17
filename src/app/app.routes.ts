f1b72723ccafa2c768261434b8478d3b858e6b0f
RGAC98 <ricardo98-avila@hotmail.com>
2024-01-08 23:31:29 -0500

    feat: components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,15 @@ import { Routes } from '@angular/router';
 
 //componentes
 import {CitasComponent} from './citas/citas.component';
+import {PacientesComponent} from './pacientes/pacientes.component';
+import {EspecialistasComponent} from './especialistas/especialistas.component';
+import {TratamientosComponent} from './tratamientos/tratamientos.component';
+import {PagosComponent} from './pagos/pagos.component';
 
 export const routes: Routes = [
-    {path: 'citas', component: CitasComponent}
+    {path: 'citas', component: CitasComponent},
+    {path: 'pacientes', component: PacientesComponent},
+    {path: 'especialistas', component: EspecialistasComponent},
+    {path: 'tratamientos', component: TratamientosComponent},
+    {path: 'pagos', component: PagosComponent}
 ];
